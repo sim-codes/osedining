4673dd23272745eef313c30cff5e914c89d2359f
Michael <segunmichael24@gmail.com>
2024-05-28 19:38:49 +0100

    db config update

--- core/settings.py
+++ core/settings.py
@@ -88,8 +88,8 @@ if DEVELOPMENT_MODE is True:
         }
     }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
+    # if os.getenv("DATABASE_URL", None) is None:
+    #     raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
