9496dc4b77a7e4e9b766fe55cc001bc313180c64
Michael <segunmichael24@gmail.com>
2024-05-28 23:29:18 +0100

    letsencrypt config

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # 3rd party apps
     'captcha',
     'bootstrapform',
+    'letsencrypt',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
 
 urlpatterns = [
+    re_path(r'^\.well-known/', include('letsencrypt.urls')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('captcha/', include('captcha.urls')),
