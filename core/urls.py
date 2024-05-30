cca5ff871e8d03b05406932324be9d3ac4b33f32
Michael <segunmichael24@gmail.com>
2024-05-30 14:52:00 +0100

    favicon update

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import re_path, path, include
 from django.views.generic.base import RedirectView
 
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
 urlpatterns = [
     re_path(r'^\.well-known/', include('letsencrypt.urls')),
+    re_path(r'^favicon\.ico$', favicon_view),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('captcha/', include('captcha.urls')),
