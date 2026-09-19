09775d02799bc0c9e7e653fad685c76c4bf816a6
Michael Ibrahim <michael.ibrahim@elios.tech>
2026-09-19 14:42:29 +0100

    feat: Update email backend configuration to use environment variables

--- core/settings.py
+++ core/settings.py
@@ -168,8 +168,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.privateemail.com'
-EMAIL_PORT = 465
+EMAIL_HOST = env('EMAIL_HOST', default='smtp.privateemail.com')
+EMAIL_PORT = env.int('EMAIL_PORT', default=465)
 EMAIL_HOST_USER = env('HOST_USER')
 EMAIL_HOST_PASSWORD = env('HOST_PASSWORD')
 # EMAIL_USE_TLS = True
